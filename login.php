9b108c7258152f6f82c6d17a7adeab656ba3ae30
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-08-30 10:14:56 -0400

    added values and a placeholder

--- login.php
+++ login.php
@@ -6,8 +6,9 @@
   </head>
   <body>
     <form method="get" action="">
-      <input type="text" name="First"> <br />
-      <input type="text" name="Last"> 
+      <input type="text" name="First" placeholder="First"> <br />
+      <input type="text" name="Last" value="LAST">
+      <input type="submit" value="go">
     </form>
   </body>
 </html>
