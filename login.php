64af719e23b72bbb3544ebd81f23906d87776fed
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-08-30 10:41:57 -0400

    added password textbox and php

--- login.php
+++ login.php
@@ -4,11 +4,25 @@
     <meta charset="utf-8">
     <title></title>
   </head>
+
+<?php
+$username = $_GET['Username'];
+$password = $_GET['Password'];
+?>
+
   <body>
     <form method="get" action="">
-      <input type="text" name="First" placeholder="First"> <br />
-      <input type="text" name="Last" value="LAST">
-      <input type="submit" value="go">
+      <input type="text" name="Username" placeholder="Username"> <br />
+      <input type="password" name="Password">
+      <br>
+      <input type="submit" value="Go">
     </form>
+
+<?php
+echo "Username was " + $username;
+echo "<br>";
+echo "Password was " + $password;
+?>
+
   </body>
 </html>
