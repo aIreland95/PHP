553aeab1121045786d626a6d6e6768458cf24a49
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-09-11 10:50:43 -0400

    additions

--- upload.php
+++ upload.php
@@ -6,9 +6,19 @@ if (!isset($_SESSION)) {
 if (!isset($_SESSION['username'])) {
   header('login.php');
 }
+
+if (isset($_POST['upload'])) {
+  $target_dir = "uploads/";
+  $target_file = $tagret_dir . basename($_FILES['upload']['name']);
+  move_uploaded_file($_FILES['upload']['tmp_namr'], $target_file);
+}
+
+
 ?>
 
 Upload your file.
-<form action="" method="post">
+<form action="" method="post" enctype="multipart/form-data">
   <input type="file" name="upload">
+  <br />
+  <input type="submit">
 </form>
