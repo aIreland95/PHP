650536fdaf554e712e4ebefedf1b17ef0cff520b
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-09-12 11:56:46 -0400

    fix?

--- upload.php
+++ upload.php
@@ -15,7 +15,7 @@ if (isset($_FILES['upload'])) {
 
   $target_dir = "uploads/";
   $target_file = $tagret_dir . basename($_FILES['upload']['name']);
-  move_uploaded_file($_FILES['upload']['name'], $target_file);
+  move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);
 }
 
 ?>
