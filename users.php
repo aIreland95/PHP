3f48f888cd2a47a7ad0dcb8a4cf248ed1b9689a0
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-09-20 10:38:03 -0400

    additional features and PHP

--- users.php
+++ users.php
@@ -1,1 +1,55 @@
- 
+<?php
+// check to see if session has started
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+// if user not logged in, sends them to login page
+if (!isset($_SESSION['username'])) {
+  header('Location: login.php');
+}
+
+// bring in the database connection
+require('dbconnection.php');
+
+// create the SQL query
+$sql = "SELECT * from users";
+
+// execute the SQL query
+$resut = $conn->query($sql);
+
+// close dbconnection
+$conn->close();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <table>
+      <tr>
+        <th>User ID </th>
+        <th>Username </th>
+        <th>Password Hash</th>
+      </tr>
+
+<?php
+// loop through all table records
+while($row = $result-fetch_assoc()) {
+  echo "<tr>";
+      echo = "<td>" . $row['userid'] . <"/td>";
+      echo = "<td>" . $row['username'] . <"/td>";
+      echo = "<td>" . $row['password'] . <"/yd>";
+  echo = "</tr>";
+}
+?>
+
+    </table>
+
+  </body>
+</html>
