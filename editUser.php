721c4c7aba59d3e1c9299227fbc70f4cc4b28842
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-09-26 10:56:14 -0400

    testing for editing user's username or password

--- editUser.php
+++ editUser.php
@@ -12,15 +12,29 @@ if (isset($_GET['id']) && ($_GET['edit']=="edit")) {
   $sql = "SELECT * FROM users WHERE user_id = " . $_GET['id']; // id is int data type, don't quote it
   $result = $conn->query($sql);
 
+  $newUser = $_GET['username'];
+  $newPass = $_GET['password'];
+
+  if (isset($_GET['id']) && ($_GET['submit']) && $_GET['username'] != null) {
+    $sql = "UPDATE users SET username = '$newUser' WHERE user_id = " . $_GET['id'];
+    header('Location: users.php');
+  }
+
+  if (isset($_GET['id']) && ($_GET['submit']) && $_GET['password'] != null) {
+    $encrypt = password_hash($newPass, PASSWORD_BCRYPT);
+    $sql = "UPDATE users SET password = '$encrypt' WHERE user_id = " . $_GET['id'];
+    header('Location: users.php');
+  }
+
   echo "<form action=\"\" method=\"post\">";
 
   while ($row = $result->fetch_assoc()) {
 
     echo "<input name=\"user_id\" type=\"text\"disabled value=\"" . $row['user_id'] . "\">";
     echo "<br />";
-    echo "<input name=\"username\" type=\"text\" value=\"" . $row['username'] . "\">";
+    echo "<input name=\"username\" type=\"text\" placeholder=\"" . $row['username'] . "\">";
     echo "<br />";
-    echo "<input name=\"password\" type=\"text\" value=\"" . $row['password'] . "\">";
+    echo "<input name=\"password\" type=\"text\" placeholder=\"" . $row['password'] . "\">";
     echo "<input type=\"submit\" name=\"submit\" value=\"change\">";
   }
 echo "</form>";
