bb5893369b4edf1e4d85f64cd4f2dbee8221b904
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-09-26 11:17:38 -0400

    database connection added to ifs

--- editUser.php
+++ editUser.php
@@ -35,11 +35,13 @@ $newUser = $_GET['username'];
 $newPass = $_GET['password'];
 
 if ($_GET['submit'] && $_GET['username'] != null) {
+  require('dbconnection.php');
   $sql = "UPDATE users SET username = '$newUser' WHERE user_id = '$oldID'";
   header('Location: users.php');
 }
 
 if ($_GET['submit'] && $_GET['password'] != null) {
+  require('dbconnection.php');
   $encrypt = password_hash($newPass, PASSWORD_BCRYPT);
   $sql = "UPDATE users SET password = '$encrypt' WHERE user_id = '$oldID'";
   header('Location: users.php');
