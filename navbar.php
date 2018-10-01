85fa9a8fba9d087a7d3bea93cf542627f43d7ae5
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-10-01 16:44:24 -0400

    test

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <?php
 echo "<br/>"
-echo (basename($_SERVER['PHP_SELF']) == "register.php") ? "<a href=register.php><strong>Register</strong></a>" : "<a href=register.php>Register</a>"
-echo (basename($_SERVER['PHP_SELF']) == "login.php") ? "<a href=login.php><strong>Login</strong></a>" : "<a href=login.php>Login</a>"
-echo (basename($_SERVER['PHP_SELF']) == "upload.php") ? "<a href=upload.php><strong>Upload</strong></a>" : "<a href=upload.php>Upload</a>"
-echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<a href=users.php><strong>Users</strong></a>" : "<a href=users.php>Users</a>"
+echo (basename($_SERVER['PHP_SELF']) == "register.php") ? "<a href=\"register.php\"><strong>Register</strong></a>" : "<a href=\"register.php\">Register</a>"
+echo (basename($_SERVER['PHP_SELF']) == "login.php") ? "<a href=\"login.php\"><strong>Login</strong></a>" : "<a href=\"login.php\">Login</a>"
+echo (basename($_SERVER['PHP_SELF']) == "upload.php") ? "<a href=\"upload.php\"><strong>Upload</strong></a>" : "<a href=\"upload.php\">Upload</a>"
+echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<a href=\"users.php\"><strong>Users</strong></a>" : "<a href=\"users.php\">Users</a>"
 ?>
