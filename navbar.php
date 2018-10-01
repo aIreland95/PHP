88516dacb2105deb05709446640bfb1e95177c5c
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-10-01 16:40:09 -0400

    forgot php tags

--- navbar.php
+++ navbar.php
@@ -1,5 +1,7 @@
+<?php
 echo "<br/>"
 echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<a href=register.php><strong>Register</strong></a>" : "<a href=register.php>Register</a>"
 echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<a href=login.php><strong>Login</strong></a>" : "<a href=login.php>Login</a>"
 echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<a href=upload.php><strong>Upload</strong></a>" : "<a href=upload.php>Upload</a>"
 echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<a href=users.php><strong>Users</strong></a>" : "<a href=users.php>Users</a>"
+?>
