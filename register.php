6bd77c0b5bf2bb3ee969032be34d3db2c33647ec
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-10-02 10:50:39 -0400

    added sanitize functions

--- register.php
+++ register.php
@@ -1,12 +1,23 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 require('dbconnection.php');
+// grab post data, can be dangerous because of XSS or MySQL Injection
 $username = $_POST['username'];
+// sanitize the $username by removing tags
+$username = filter_var($username, FILTER_SANITIZE_STRING);
+// trim any whitespace from beginning and end of $username
+$username = trim($username);
+// take off the lashes in an inout box
+$username = stripslashes($username);
+// take off blank spaces within the username
+$username = str_replace(' ','',$username);
+// grab post data...password will be hashed, so no need to sanitize
 $password = $_POST['password'];
 $password = password_hash($password, PASSWORD_BCRYPT);
 $sql = "INSERT INTO users (username,password) VALUES ('$username','$password')";
 $conn->query($sql);
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
