7dee3b88410b6e8af8b4d040f6c54d3fb4891cb3
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-10-04 10:14:26 -0400

    setting up a test for our cookie

--- cookies.php
+++ cookies.php
@@ -1,7 +1,26 @@
 <?php
-// cookies rae used to identify users, can be created using setcookie() function
+// cookies are used to identify users, can be created using setcookie() function
 // we'll use name, value, and expire for the function
-
-
-
+$cookie_name = "user";
+$cookie_value = "bob";
+// setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+// 86400 = 1 day
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      if (isset($_COOKIE['user'])) {
+        echo "You have been here before.";
+      }
+      else {
+        echo "This is your first time here.";
+      }
+    ?>
+  </body>
+</html>
