d8d71ef6f9f8fad58579f1a848b7f6bd7b86126e
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-10-04 10:22:53 -0400

    test

--- cookies.php
+++ cookies.php
@@ -15,13 +15,12 @@ $cookie_value = "bob";
   <body>
     <?php
       if (isset($_COOKIE['user'])) {
-        echo "You have been here before.";
+        echo "You have been here before \`[-|-]/";
       }
       else {
         echo "This is your first time here.";
         // setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
       }
-      echo "\`[-|-]/"
     ?>
   </body>
 </html>
