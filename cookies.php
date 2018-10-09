2b51dc6effcedea5f5852556a13427ee1d5304cd
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-10-08 22:23:49 -0400

    changing things up again

--- cookies.php
+++ cookies.php
@@ -4,8 +4,8 @@
 
 // cookies are used to identify users, can be created using setcookie() function
 // we'll use name, value, and expire for the function
-$cookie_name = "user";
-$cookie_value = "bob";
+$cookie_name = "visit";
+$cookie_value = date("g:i:s - m/d/y");
 // 86400 = 1 day
 ?>
 
@@ -17,14 +17,13 @@ $cookie_value = "bob";
   </head>
   <body>
     <?php
-      if (isset($_COOKIE['user'])) {
+      if (isset($_COOKIE['visit'])) {
+
+        $seconds = $_COOKIE['visit'];
+        $secondsCalc = (time() - $seconds);
 
         echo "You have been here before.";
-        echo "<br> Your last visit was - " . $visit;
-
-        $seconds = $_COOKIE['lastVisit'];
-        $secondsCalc = ($seconds - date());
-
+        echo "<br> Your last visit was - " . $cookie_value;
         echo "<br> It has been " . $secondsCalc . " seconds since you reloaded this page.";
         echo "<br> \`[-|-]/";
 
@@ -35,7 +34,6 @@ $cookie_value = "bob";
       }
       date_default_timezone_set('America/New_York');
       setcookie($cookie_name, $cookie_value, time() + (60), "/");
-      setcookie('lastVisit', date("g:i:s - m/d/y"), time() + (60), "/");
     ?>
   </body>
 </html>
