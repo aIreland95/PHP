91c12808adc4baf084ce7f5f7d63beb3ea59546c
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-10-10 11:45:55 -0400

    cleaned up the code a bit

--- shell_exec.php
+++ shell_exec.php
@@ -11,15 +11,20 @@ echo "<pre>$pwd</pre>";
 $check = file_exists("test");
 
 if ($check) {
+
   $checkDir = is_dir("test");
+
   if ($checkDir) {
+
     echo "This exists and is a directory!";
   }
   else {
+
     echo "This exists, but it's not a directory.";
   }
 }
 else {
+
   mkdir("test");
 }
 
