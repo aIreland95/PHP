ae56ae77f928bd471c22b3bbc03df813590b8e3a
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-10-10 11:38:33 -0400

    only removed $goodCheck

--- shell_exec.php
+++ shell_exec.php
@@ -9,12 +9,8 @@ $pwd = shell_exec('pwd');
 echo "<pre>$pwd</pre>";
 
 $check = file_exists("test");
-$goodCheck = file_exists("uploads");
 
 if ($check == false) { echo "This file does not exist..."; }
-else { echo "This file exists!"; }
-
-if ($goodCheck == false) { echo "This file does not exist..."; }
 else { echo "This file exists!"; }
 
  ?>
