3de3102643415906ea88cf61fcface3ef165a82b
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-10-10 11:22:19 -0400

    testing if a file exists

--- shell_exec.php
+++ shell_exec.php
@@ -1,7 +1,13 @@
 <?php
+
+// check if folder "test" exists
+
 $output = shell_exec('ls -lah');
 echo "<pre>$output</pr>";
 
 $pwd = shell_exec('pwd');
 echo "<pre>$pwd</pre>";
+
+$check = shell_exec('find /var/www/html/aaron/PHP "test"');
+echo "<pre>$check</pre>";
  ?>
