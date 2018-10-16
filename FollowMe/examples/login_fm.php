ff9402e4df6f0c4287207b8144ca24ffbf12ce59
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-10-16 19:35:40 -0400

    empty page, this is where register will redirect to

---
+++ FollowMe/examples/login_fm.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>Our login page is under construction, come back later \`[-|-]/
+  </body>
+</html>
