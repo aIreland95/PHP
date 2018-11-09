6dbef38adc8c7074a8a849372f0f107b065a6d71
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-11-09 18:53:48 -0500

    renaming

--- FollowMe/examples/profile.php
+++ FollowMe/examples/profile.php
@@ -160,21 +160,21 @@ $result = $conn->query($sql);
                       $follow_result = $conn->query($follow_sql);
 
                       while($row = $follow_result->fetch_row()) {
-                        $following_user_ids[] = $row2[0];
+                        $following_user_ids[] = $row[0];
                       }
 
-                      while($row2 = $follow_result->fetch_assoc()) {
+                      while($row = $follow_result->fetch_assoc()) {
 
-                        $user_id = $row2['user_id'];
+                        $user_id = $row['user_id'];
 
                         if (in_array($user_id, $following_user_ids)) {
                           echo "<li>";
               						echo	"<div class=\"row\">";
               						echo		"<div class=\"col-md-2 col-sm-2 ml-auto mr-0\">";
-              						echo			"<img src=" . $row2['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
+              						echo			"<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
               						echo		"</div>";
               						echo		"<div class=\"col-md-7 col-sm-4  ml-0 mr-0\">";
-              						echo			"<h6>" . $row2['first_name'] . " " . $row2['last_name'] . "<br/><small>" . $row2['title'] . "</small></h6>";
+              						echo			"<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
               						echo		"</div>";
               						echo	"</div>";
               						echo "</li>";
