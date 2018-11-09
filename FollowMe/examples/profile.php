20075eae0031aa628eaefc23193db060e58034db
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-11-09 10:13:41 -0500

    test

--- FollowMe/examples/profile.php
+++ FollowMe/examples/profile.php
@@ -171,7 +171,7 @@ while($row = $follow_result->fetch_row()) {
                             </div>
                         </div>
                     </div>
-                    <div class="tab-pane" id="following" role="tabpanel">
+                    <div class="tab-pane text-center" id="following" role="tabpanel">
                       <div class="row">
                           <div class="col-md-6 ml-auto mr-auto">
                             <ul class="list-unstyled follows">
