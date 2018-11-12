2e666831299c3b83cadec26455bc920b2a15e45d
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-11-12 11:34:57 -0500

    test

--- FollowMe/examples/profile.php
+++ FollowMe/examples/profile.php
@@ -162,27 +162,25 @@ $result = $conn->query($sql);
                                 $following_user_ids[] = $row[0];
                               }
 
-                              foreach ($following_user_ids as $key => $value) {
-                                echo $value;
+                              while($row = $result->fetch_assoc()) {
+
+                                $user_id = $row['user_id'];
+
+                                if (in_array($user_id, $following_user_ids)) {
+                                  // echo "<li>";
+                                  // echo	"<div class=\"row\">";
+                                  // echo		"<div class=\"col-md-2 col-sm-2 ml-auto mr-0\">";
+                                  // echo			"<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
+                                  // echo		"</div>";
+                                  // echo		"<div class=\"col-md-7 col-sm-4  ml-0 mr-0\">";
+                                  // echo			"<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
+                                  // echo		"</div>";
+                                  // echo	"</div>";
+                                  // echo "</li>";
+                                  // echo "<hr />";
+                                  echo $user_id;
+                                }
                               }
-                              // while($row = $result->fetch_assoc()) {
-                              //
-                              //   $user_id = $row['user_id'];
-                              //
-                              //   if (in_array($user_id, $following_user_ids)) {
-                              //     echo "<li>";
-                              //     echo	"<div class=\"row\">";
-                              //     echo		"<div class=\"col-md-2 col-sm-2 ml-auto mr-0\">";
-                              //     echo			"<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
-                              //     echo		"</div>";
-                              //     echo		"<div class=\"col-md-7 col-sm-4  ml-0 mr-0\">";
-                              //     echo			"<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
-                              //     echo		"</div>";
-                              //     echo	"</div>";
-                              //     echo "</li>";
-                              //     echo "<hr />";
-                              //   }
-                              // }
 
                               ?>
                           </ul>
