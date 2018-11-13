d396a8df299a393ec5696f3783c3362105e10f4f
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-11-13 10:46:25 -0500

    test

--- FollowMe/examples/editprofile.php
+++ FollowMe/examples/editprofile.php
@@ -7,20 +7,17 @@ if (!isset($_SESSION)) {
 }
 require('database.php');
 
+if (!file_exists("../assets/img/faces/". $_SESSION['user_id'])) {
+  mkdir("../assets/img/faces/" . $_SESSION['user_id'], 0777, true);
+}
+
 if (isset($_SESSION['email']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
 
 if (isset($_POST['update-btn']) && $_POST['first_name'] != null && $_POST['last_name'] != null && $_POST['title'] != null && $_POST['description'] != null) {
 
-  if (isset($_FILES['faces'])) {
-      if (!file_exists("../assets/img/faces/". $_SESSION['user_id'])) {
-        mkdir("../assets/img/faces/" . $_SESSION['user_id'], 0777, true);
-      }
-
       $target_dir = "../assets/img/faces/" . $_SESSION['user_id'] . "/";
       $target_file = $target_dir . basename($_FILES['faces']['user_id']);
       $uploadVerify = true;
-    }
-
 
     $email = $_SESSION['email'];
     $firstname = $_POST['first_name'];
