9ed3ea8e70dc2e29b936c5eb55d8fdfde81b83de
aIreland95 <42806032+aIreland95@users.noreply.github.com>
2018-11-13 10:31:46 -0500

    revert, page wouldn't load

--- FollowMe/examples/editprofile.php
+++ FollowMe/examples/editprofile.php
@@ -11,21 +11,15 @@ if (isset($_SESSION['email']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
 
 if (isset($_POST['update-btn']) && $_POST['first_name'] != null && $_POST['last_name'] != null && $_POST['title'] != null && $_POST['description'] != null) {
 
-  if (isset($_FILES['upload'])) {
-    // check to see if uploads folder exists
-  if (!file_exists("uploads")) {
+  if (isset($_FILES['faces'])) {
+      if (!file_exists("../assets/img/faces/". $_SESSION['user_id'])) {
+        mkdir("../assets/img/faces/" . $_SESSION['user_id'], 0777, true);
+      }
 
-      //if uploads folder doesn't exist, create it
-      mkdir("./uploads");
-  } 
-
-  if (!file_exists("uploads/" . $_SESSION['user_id'])) {
-    mkdir("uploads/" . $_SESSION['user_id'], 0777, true);
-  }
-
-    $target_dir = "uploads/" . $_SESSION['user_id'] . "/";
-    $target_file = $target_dir . basename($_FILES['upload']['name']);
-    $uploadVerify = true;
+      $target_dir = "../assets/img/faces/" . $_SESSION['user_id'] . "/";
+      $target_file = $target_dir . basename($_FILES['faces']['user_id']);
+      $uploadVerify = true;
+    }
 
 
     $email = $_SESSION['email'];
@@ -33,7 +27,7 @@ if (isset($_POST['update-btn']) && $_POST['first_name'] != null && $_POST['last_
     $lastname = $_POST['last_name'];
     $title = $_POST['title'];
     $description = $_POST['description'];
-    $image = $_POST['image_url']; // latest addition
+    $image = $target_file; // latest addition
 
     $sql = "UPDATE fm_users SET first_name = '$firstname', last_name = '$lastname', image_url = '$image', title = '$title', description = '$description' WHERE email = '$email'";
     $conn->query($sql);
